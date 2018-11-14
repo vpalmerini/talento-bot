25d55efc51bfacada41079ef0eccf87c795c39e8
vpalmerini <victorpalmerini@gmail.com>
2018-11-14 20:53:20 -0200

    add json module

--- trello_bot/bot/views.py
+++ trello_bot/bot/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+import json
 
 file_token = open('token.txt', 'r')
 token = file_token.read()
@@ -12,7 +13,10 @@ key = file_key.read()
 @csrf_exempt
 def main(request):
 
+	print('main')
+
 	if request.method == 'POST':
+		print('post')
 		try:
 			body_unicode = request.body.decode('utf-8')
 			data = json.loads(body_unicode)
