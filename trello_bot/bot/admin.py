af70cc52b2951c9267cebf4e6016ba856aa3cab3
vpalmerini <victorpalmerini@gmail.com>
2018-11-18 21:46:20 -0200

    add models Hunter and Company

--- trello_bot/bot/admin.py
+++ trello_bot/bot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Hunter, Company
 
-# Register your models here.
+admin.site.register(Hunter)
+admin.site.register(Company)
--- trello_bot/bot/models.py
+++ trello_bot/bot/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Hunter(models.Model):
+
+	email = models.EmailField(max_length=100, primary_key=True)
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Company(models.Model):
+
+	name = models.CharField(max_length=100)
+
+	FINANCIAL = 'FN'
+	CONSULTING = 'CS'
+	INDUSTRY = 'ID'
+
+	CATEGORY_CHOICES = (
+		(FINANCIAL,'Financial'),
+		(CONSULTING,'Consulting'),
+		(INDUSTRY,'Industry')
+	)
+
+	category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+
+	CONTACTED = 'CT'
+	INTERESTED = 'IT'
+	DECLINED = 'DC'
+	CLOSED = 'CL'
+	SIGNED = 'SG'
+	PAID = 'PD'
+
+	STATUS_CHOICES = (
+		(CONTACTED, 'Contacted'),
+		(INTERESTED,'Interested'),
+		(DECLINED,'Declined'),
+		(CLOSED,'Closed'),
+		(SIGNED,'Signed'),
+		(PAID,'Paid')
+	)
+
+	status = models.CharField(max_length=2, choices=STATUS_CHOICES, blank=True)
+
+	hunter = models.ForeignKey('Hunter', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
